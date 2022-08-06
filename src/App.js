ea0e32b45e5e5c2857832c00634ed11b914a6099
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-08-06 15:53:53 +1000

    Adding Pacman Photo

--- src/App.js
+++ src/App.js
@@ -4,37 +4,40 @@ import "./Dictionary.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="DictionaryHeader">Search for a Word</header>
-        <br></br>
-        <main>
-          <Dictionary />
-        </main>
-        <footer>
-          <small>
-            {" "}
-            This project open sourced on{" "}
-            <a
-              href="https://github.com/strigenzc/react-dictionary"
-              target="_blank"
-              rel="nooperner noreferrer"
-            >
+    <div container="webpage">
+      <div className="App">
+        <div className="container">
+          <header className="DictionaryHeader">Search for a Word</header>
+          <br></br>
+          <main>
+            <Dictionary />
+          </main>
+          <footer>
+            <small>
               {" "}
-              Github
-            </a>{" "}
-            <br></br>
-            Portfolio found{" "}
-            <a
-              href="https://strigenz-portfolio.netlify.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-          </small>
-        </footer>
+              This project open sourced on{" "}
+              <a
+                href="https://github.com/strigenzc/react-dictionary"
+                target="_blank"
+                rel="nooperner noreferrer"
+              >
+                {" "}
+                Github
+              </a>{" "}
+              <br></br>
+              Portfolio found{" "}
+              <a
+                href="https://strigenz-portfolio.netlify.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+            </small>
+          </footer>
+        </div>
       </div>
+      <img src="pacman.jpg" alt="pacman photo" />
     </div>
   );
 }
