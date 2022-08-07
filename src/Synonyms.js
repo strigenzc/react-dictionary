ba6b4787ca6c036f6d0a5cfbf6317314385fda89
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-08-07 19:25:27 +1000

    More CSS

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Dictionary.css";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
+      <ul className="Synonym">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
